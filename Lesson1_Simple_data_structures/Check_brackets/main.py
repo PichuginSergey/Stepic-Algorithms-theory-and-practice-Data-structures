ece240849d7199439d540cac3ff5a8c3dc2268bb
PichuginSergey <PichuginSM@ya.ru>
2017-05-05 21:31:33 +0300

    Added the solution of the first task. It is checked brackets in message.

---
+++ Lesson1_Simple_data_structures/Check_brackets/main.py
@@ -0,0 +1,24 @@
+def check_brackets(msg):
+    stack = []
+    for idx in range(len(msg)):
+        elm = msg[idx]
+        if (elm in ('(', '{', '[')):
+            stack.append((elm, idx+1))
+        elif (elm in (')', '}', ']')):
+            if len(stack) == 0:
+                print idx+1
+                return
+            if (elm == ')' and stack[-1][0] == '(') or \
+               (elm == '}' and stack[-1][0] == '{') or \
+               (elm == ']' and stack[-1][0] == '['):
+                stack.pop()
+            else:
+                print idx+1
+                return
+    if len(stack) == 0:
+        print 'Success'
+    else:
+        print stack[-1][1]
+
+msg = input()
+check_brackets(msg)
